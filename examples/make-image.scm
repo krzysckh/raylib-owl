34bcd77407cc01d275dd8bac7a405e29b016a46b
krzysckh <kpm@linux.pl>
2024-09-16 08:34:34 +0200

    bump owl version, pre convert to bv in example

--- examples/make-image.scm
+++ examples/make-image.scm
@@ -5,12 +5,13 @@
 
 (define (f x y) (band x y))
 
-(define l (let loop ((x 0) (y 0) (acc #n))
-            (cond
-             ((>= y 50) acc)
-             ((>= x 50) (print y) (loop 0 (+ y 1) acc))
-             (else
-              (loop (+ x 1) y (append acc (if (= (f x y) 0) '(0 0 0 255) '(255 255 255 255))))))))
+(define bv (list->bytevector
+            (let loop ((x 0) (y 0) (acc #n))
+              (cond
+               ((>= y 50) acc)
+               ((>= x 50) (print y) (loop 0 (+ y 1) acc))
+               (else
+                (loop (+ x 1) y (append acc (if (= (f x y) 0) '(0 0 0 255) '(255 255 255 255)))))))))
 
 (define texture-filters
   (vector
@@ -26,8 +27,7 @@
   (set-target-fps! 30)
   (with-window
    400 400 "make-image example"
-   (let* ((bv (list->bytevector l))
-          (i (image-color-replace (image-color-invert (make-image 50 50 bv)) white red))
+   (let* ((i (image-color-replace (image-color-invert (make-image 50 50 bv)) white red))
           (t (image->texture i)))
 
      (example (len (image->list-palette i 10)) = 2)
